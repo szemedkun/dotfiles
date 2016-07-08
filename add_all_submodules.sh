8e9c527986321194b02513ffa444949dc8449779
szemedkun <szemedkun@gmail.com>
2016-07-08 14:12:54 -0600

    auto installation fix

--- add_all_submodules.sh
+++ add_all_submodules.sh
@@ -1,9 +1,10 @@
 mkdir ~/.vim/autoload
-git submodule add  https://github.com/tpope/vim-pathogen ~/.vim/autoload/pathogen.vim
-git submodule add  https://github.com/tomtom/tlib_vim ~/.vim/bundle/tlib_vim
-git submodule add  https://github.com/MarcWeber/vim-addon-mw-utils ~/.vim/bundle/vim-addon-mw-utils 
-git submodule add  https://github.com/garbas/vim-snipmate ~/.vim/bundle/vim-snipmate
-git submodule add  https://github.com/honza/vim-snippets ~/.vim/bundle/vim-snippets
-git submodule add  https://github.com/SirVer/ultisnips ~/.vim/bundle/ultisnips
-git submodule add  https://github.com/tpope/vim-commentary ~/.vim/bundle/commentary
-
+git clone  https://github.com/tpope/vim-pathogen ~/.vim/autoload/pathogen
+cp ~/.vim/autoload/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+git clone  https://github.com/tomtom/tlib_vim ~/.vim/bundle/tlib_vim
+git clone  https://github.com/MarcWeber/vim-addon-mw-utils ~/.vim/bundle/vim-addon-mw-utils 
+git clone  https://github.com/garbas/vim-snipmate ~/.vim/bundle/vim-snipmate
+git clone  https://github.com/honza/vim-snippets ~/.vim/bundle/vim-snippets
+git clone  https://github.com/SirVer/ultisnips ~/.vim/bundle/ultisnips
+git clone  https://github.com/tpope/vim-commentary ~/.vim/bundle/commentary
+git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/easymotion
